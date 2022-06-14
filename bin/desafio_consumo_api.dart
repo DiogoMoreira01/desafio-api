76fa411180eb3ac4cf1a9bafa14bd89439f474ee
Diogo <dyogomoreira01@gmail.com>
2022-06-14 11:45:26 -0300

    001

---
+++ bin/desafio_consumo_api.dart
@@ -0,0 +1,5 @@
+import 'package:desafio_consumo_api/desafio_consumo_api.dart' as desafio_consumo_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${desafio_consumo_api.calculate()}!');
+}
